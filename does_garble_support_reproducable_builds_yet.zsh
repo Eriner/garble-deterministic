e12f8858721228023a3127797694cab92a7ee793
Matt Hamilton <m@tthamilton.com>
2021-05-03 09:22:24 -0400

    still not deterministic w/o -literals, -tiny
    Signed-off-by: Matt Hamilton <m@tthamilton.com>

--- does_garble_support_reproducable_builds_yet.zsh
+++ does_garble_support_reproducable_builds_yet.zsh
@@ -9,7 +9,7 @@ fi
 _out=$(mktemp -d)
 
 _build() {
-  CGO_ENABLED=0 GOOS=linux GOARCH=amd64 garble -literals -seed="$(echo abcdefghijklmnop | base64)" -tiny build -o ${_out}/garble-test .
+  GOPRIVATE='*' CGO_ENABLED=0 GOOS=linux GOARCH=amd64 garble -seed="$(echo abcdefghijklmnop | base64)" build -o ${_out}/garble-test .
   shasum -a 256 ${_out}/garble-test
   rm -f ${_out}/garble-test
 }
